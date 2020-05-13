09b8cab0b0d6069e6edb9c2dd08d9cabcd98904b
thauandev <thauandepaula@icloud.com>
2020-05-13 11:16:00 -0300

    Criando a rota de get e post de transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,35 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepo = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepo.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepo.save(transaction);
+
+    return transaction;
   }
 }
 
