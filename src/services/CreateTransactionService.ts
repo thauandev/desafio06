64e3738dcba9bdcff5f9fa7748204cb323985775
thauandev <thauandepaula@icloud.com>
2020-05-13 14:52:44 -0300

    Criando o Delete

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,8 +1,9 @@
-// import AppError from '../errors/AppError';
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
 interface Request {
@@ -20,11 +21,32 @@ class CreateTransactionService {
     category,
   }: Request): Promise<Transaction> {
     const transactionsRepo = getCustomRepository(TransactionsRepository);
+    const categoryRepo = getRepository(Category);
+
+    const { total } = await transactionsRepo.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let transactionCategory = await categoryRepo.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepo.create({
+        title: category,
+      });
+      await categoryRepo.save(transactionCategory);
+    }
 
     const transaction = transactionsRepo.create({
       title,
       value,
       type,
+      category: transactionCategory,
     });
 
     await transactionsRepo.save(transaction);
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepo = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionRepo.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepo.remove(transaction);
   }
 }
 
