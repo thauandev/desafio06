a58d7d158f1c8697fe791585920dc9001f902cb0
thauandev <thauandepaula@icloud.com>
2020-05-14 14:40:57 -0300

    Implementando o import

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'gostack_desafio06_test'
+          ? 'gostack_desafio06_tests'
           : defaultOptions.database,
     }),
   );
